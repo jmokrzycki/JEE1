141f748ab9831645aac8c1828c71c9e1528ce9b3
jmokrzycki <jmokrzycki@gmail.com>
2015-11-06 18:12:43 +0100

    Dodanie klasy Rezyser.java

--- src/main/java/com/example/servletjspdemo/service/StorageService.java
+++ src/main/java/com/example/servletjspdemo/service/StorageService.java
@@ -4,68 +4,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.example.servletjspdemo.domain.Film;
+import com.example.servletjspdemo.domain.Rezyser;
 
 public class StorageService {
 
 	private List<Film> db = new ArrayList<Film>();
 
-	public void add(Film film){
-		Film newFilm = new Film(film.getTytul(), film.getRok_premiery());
-		db.add(newFilm);
-	}
+    public void add(Film film){
+        Film newFilm = new Film(film.getTytul(), film.getRok_premiery());
+        db.add(newFilm);
+    }
 
-	public List<Film> getAllPersons(){
-		return db;
-	}
+    public List<Film> getAllFilm(){
+        return db;
+    }
+
+
+
+   //private List<Rezyser> db = new ArrayList<Rezyser>();
+
+   //public void add(Rezyser rezyser){
+   //    Rezyser newRezyser = new Rezyser(rezyser.getImie(), rezyser.getNazwisko());
+   //    db.add(newRezyser);
+   //}
+
+   //public List<Rezyser> getAllRezyser(){
+   //        return db;
+   //}
 
 }
 
-/*
-public class StorageService {
-	
-	private List<Rezyser> db = new ArrayList<Rezyser>();
-	
-	public void add(Rezyser person){
-		Rezyser newPerson = new Rezyser(person.getTytul(), person.getRok_premiery());
-		db.add(newPerson);
-	}
-	
-	public List<Rezyser> getAllPersons(){
-		return db;
-	}
-
-}
-*/
-
-/*
-package com.example.servletjspdemo.domain;
-
-public class Rezyser {
-
-    private String imie = "Jan";
-    private String nazwisko = "Kowalski";
-
-    public Rezyser() {
-        super();
-    }
-
-    public Rezyser(String imie, String nazwisko) {
-        super();
-        this.imie = imie;
-        this.nazwisko = nazwisko;
-    }
-
-    public String getImie() {
-        return imie;
-    }
-    public void setImie(String imie) {
-        this.imie = imie;
-    }
-    public String getNazwisko() {
-        return nazwisko;
-    }
-    public void setNazwisko(String nazwisko) {
-        this.nazwisko = nazwisko;
-    }
-}
- */
